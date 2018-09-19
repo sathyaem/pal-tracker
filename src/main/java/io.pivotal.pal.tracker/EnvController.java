d6b2cfe90ad1fe1af63b2e621859dbd3b3d58e97
sathyaem <sathiasara@gmail.com>
2018-09-19 09:42:54 +0530

    last commit

--- src/main/java/io.pivotal.pal.tracker/EnvController.java
+++ src/main/java/io.pivotal.pal.tracker/EnvController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 @RestController
 public class EnvController {
 
-    
+    private final String test="c";
     private final String port;
     private final String memoryLimit;
     private final String cfInstanceIndex;
